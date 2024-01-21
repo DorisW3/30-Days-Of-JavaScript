35c8889c8c44c563bdab754075c87e47738d35fa
Doris Wiebe <doris-wiebe@gmx.de>
2024-01-21 17:40:49 +0100

    day 2 - exercise: level 2

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -52,3 +52,54 @@ const secondPart = "JavaScript";
 console.log(firstPart.concat(secondPart));
 
 console.log(challenge.repeat(2));
+
+// ### Exercise: Level 2
+
+console.log(
+  "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+);
+
+console.log(
+  " \"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\" - Mother Teresa"
+);
+
+console.log(typeof "10" === 10);
+console.log(parseInt("10") === 10);
+
+console.log(parseFloat("9.81") === 10);
+console.log(Math.round("9.81") === 10);
+
+const language = "python";
+const slang = "jargon";
+console.log("include on: ", language.includes("on"), slang.includes("on"));
+
+const sentence = "I hope this course is not full of jargon.";
+console.log("jargon: ", sentence.includes("jargon"));
+
+console.log(Math.random() * 101);
+console.log(Math.random() * 101 + 50);
+console.log(Math.random() * 101 + 155);
+
+const stringCharacter = "Javascript";
+console.log(
+  "random charakter ",
+  stringCharacter[Math.floor(Math.random() * stringCharacter.length)]
+);
+
+const row1 = "1\t1\t1\t1\t1";
+const row2 = "2\t1\t2\t4\t8";
+const row3 = "3\t1\t3\t9\t27";
+const row4 = "4\t1\t4\t16\t64";
+const row5 = "5\t1\t5\t25\t125";
+
+console.log(row1);
+console.log(row2);
+console.log(row3);
+console.log(row4);
+console.log(row5);
+
+const sliceOut =
+  "You cannot end a sentence with because because because is a conjunction.";
+console.log(sliceOut.indexOf("because"));
+console.log(sliceOut.lastIndexOf("because") + 7);
+console.log(sliceOut.substring(31, 54));
